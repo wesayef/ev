77fb03e8270f1362fb23c52901c93d6d2115dd49
AhlamHasan <69400247+AhlamHasan@users.noreply.github.com>
2021-08-12 02:23:47 +0300

    Update login.php

--- templates/login.php
+++ templates/login.php
@@ -71,7 +71,7 @@ if(isset($_POST['login_now']))
   <title>
 
   </title>
-  <link href="http://localhost/ev/templates/assets/css/style_all_ar.css" rel="stylesheet">
+  <link href="../templates/assets/css/style_all_ar.css" rel="stylesheet">
 
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
