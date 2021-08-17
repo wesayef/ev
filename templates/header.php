f588fb7261abfeb08843d3dfa3925a42e2c3f691
GhaidaKS <66637334+GhaidaKS@users.noreply.github.com>
2021-08-17 19:46:00 +0300

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -45,7 +45,7 @@ else
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/png" sizes="16x16" href="">
   <title></title>
-  <link href="http://localhost/ev/templates/assets/css/style_all_ar.css" rel="stylesheet">
+  <link href="../ev/templates/assets/css/style_all_ar.css" rel="stylesheet">
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
