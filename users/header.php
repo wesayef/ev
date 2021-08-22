8b157e76af2327c37b1516e628df691d4a7135f0
AhlamHasan <69400247+AhlamHasan@users.noreply.github.com>
2021-08-22 19:03:11 +0300

    Update header.php

--- users/header.php
+++ users/header.php
@@ -11,6 +11,10 @@
     أرشدني
   </title>
   <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-title" content="Mobile rules">
+  <meta name="apple-mobile-web-app-status-bar-style" content="red">
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
