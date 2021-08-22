1263d0bfb9fe0e3432c8c07bb65699928fa6e9eb
AhlamHasan <69400247+AhlamHasan@users.noreply.github.com>
2021-08-22 19:09:03 +0300

    Update header.php

--- users/header.php
+++ users/header.php
@@ -11,6 +11,7 @@
     أرشدني
   </title>
   <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="apple-mobile-web-app-capable" content="yes" />
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-title" content="Mobile rules">
