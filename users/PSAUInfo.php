030cc34863a39ea924cefa34b0102fe2ab1a1483
wesayef <63670034+wesayef@users.noreply.github.com>
2021-08-27 15:08:32 +0300

    Create PSAUInfo.php

---
+++ users/PSAUInfo.php
@@ -0,0 +1,111 @@
+<html style="font-size: 16px;">
+  
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="​Our relentless focus on innovation, luxury, and sustainability&nbsp;moves us toward a future where you no&nbsp;longer have to ​choose between doing&nbsp;great things and doing the right thing., We help and solve your business problem., ​We help your business to grow, Projec​ts, ​Benefits of Coaching for Individuals., About Our Team">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Page 2</title>
+    <link rel="stylesheet" href="nicepageT2.css" media="screen">
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Page-2.css" media="screen">
+<link rel="stylesheet" href="Page-4.css" media="screen">
+    <meta name="generator" content="Nicepage 3.23.2, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i">
+   
+  </head>
+  
+
+<body class="u-body">
+        <section class="u-clearfix u-section-1" id="sec-4a54">
+          <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+            <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+              <div class="u-layout">
+                <div class="u-layout-row">
+                  <div class="u-container-style u-image u-layout-cell u-left-cell u-size-41 u-image-1" data-image-width="1280" data-image-height="853">
+                    <div class="u-container-layout u-container-layout-1"></div>
+                  </div>
+                  <div class="u-container-style u-layout-cell u-right-cell u-size-19 u-layout-cell-2">
+                    <div class="u-container-layout u-valign-bottom-lg u-valign-middle-xl u-container-layout-2">
+                      <div class="u-border-2 u-border-grey-25 u-container-style u-expanded-width-xs u-group u-white u-group-1">
+                        <div class="u-container-layout u-valign-middle u-container-layout-3">
+                          <h3 class="u-text u-text-default u-text-palette-5-dark-2 u-text-1">جامعة الأمير سطام بن عبدالعزيز</h3>
+                          <p class="u-text u-text-default ">(جامعة الأمير سلمان بن عبد العزيز وجامعة الخرج سابقًا) هي جامعة حكومية سعودية تقع في مدينة السيح بمحافظة الخرج بالمملكة العربية السعودية وهي تحت إشراف وزارة التعليم السعودية وتولى إدارة الجامعة منذ تأسيسها الدكتور عبد الرحمن بن محمد بن مصدي العاصمي وقد تعين بعد ذلك نائباً لوزير التعليم، وقد كلّف الدكتور عبد العزيز عبد الله الحامد مديراً للجامعة</p>
+                          <a href="#" class="u-border-2 u-border-palette-4-light-1 u-link u-no-underline u-text-hover-palette-4-light-1 u-text-palette-5-dark-2 u-link-1">read more</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+    
+    
+    
+      </div>
+    <section class="u-align-left u-clearfix u-grey-5 u-section-2" id="carousel_e0eb">
+        <div class="u-clearfix u-sheet u-sheet-1">
+          <h2 class="u-custom-font u-font-montserrat u-text u-text-1">تخصصات جامعة الأمير سطام بن عبدالعزيز</h2>
+          <div class="u-border-16 u-border-custom-color-1 u-line u-line-horizontal u-line-1"></div>
+          <div class="u-expanded-width u-list u-list-1">
+            <div class="u-repeater u-repeater-1">
+
+              <div class="u-align-left u-container-style u-custom-item u-list-item u-repeater-item u-white u-list-item-1 ">
+                <div class="u-container-layout u-similar-container u-container-layout-1"><span class="u-align-left u-border-2 u-border-palette-2-base u-icon u-icon-circle u-spacing-24 u-text-custom-color-1 u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 24 24" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-29d3"></use></svg><svg class="u-svg-content" viewBox="0 0 24 24" id="svg-29d3"><path d="m12 6c-1.228 0-12-.084-12-3s10.772-3 12-3 12 .084 12 3-10.772 3-12 3zm-10.412-3c.732.568 4.245 1.5 10.412 1.5s9.68-.932 10.412-1.5c-.732-.568-4.245-1.5-10.412-1.5s-9.68.932-10.412 1.5zm20.939.116h.01z"></path><path d="m12 12c-1.228 0-12-.084-12-3 0-.414.336-.75.75-.75.385 0 .702.29.745.664.462.553 4.012 1.586 10.505 1.586s10.043-1.033 10.505-1.586c.043-.374.36-.664.745-.664.414 0 .75.336.75.75 0 2.916-10.772 3-12 3zm10.5-3.001c0 .001 0 .001 0 0zm-21 0c0 .001 0 .001 0 0z"></path><path d="m12 18c-1.228 0-12-.084-12-3 0-.414.336-.75.75-.75.385 0 .702.29.745.664.462.553 4.012 1.586 10.505 1.586s10.043-1.033 10.505-1.586c.043-.374.36-.664.745-.664.414 0 .75.336.75.75 0 2.916-10.772 3-12 3zm10.5-3.001c0 .001 0 .001 0 0zm-21 0c0 .001 0 .001 0 0z"></path><path d="m12 24c-1.228 0-12-.084-12-3v-18c0-.414.336-.75.75-.75s.75.336.75.75v17.919c.481.556 4.03 1.581 10.5 1.581s10.019-1.025 10.5-1.581v-17.919c0-.414.336-.75.75-.75s.75.336.75.75v18c0 2.916-10.772 3-12 3z"></path><circle cx="5" cy="14" r="1"></circle><circle cx="5" cy="8" r="1"></circle><circle cx="5" cy="20" r="1"></circle></svg></span>
+                  <h3 class="u-text u-text-default-lg u-text-default-md u-text-default-xl u-text-2">علوم الحاسب</h3>
+                  <p class="u-text u-text-3">علم تحليل وتصميم الأنظمة والخوارزميات من خلال مهارات التحليل والتصميم والبرهنة والمقارنة وتحليل الخوارزميات ، تحليل الأنظمة ، حل المسائل ،التحليل العددي ،خوارزميات المخططات ، قياس أداء الحاسوب ، تمييز الأنماط ، معالجة الصور ، استخدام الذكاء الإصطناعي ، البحث والفرز ، تنظيم مكونات الحاسوب وإدارتها ، تصميم لغات البرمجة&nbsp;</p>
+                  <div class="dropdown u-text u-text-3">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <b>المجالات الوظيفية</b>
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a class="dropdown-item" href="#">مطور تطبيقات</a>
+                      <a class="dropdown-item" href="#">محلل بيانات</a>
+                      <a class="dropdown-item" href="#">مهندس شبكات</a>
+                      <a class="dropdown-item" href="#">مهندس برمجيات</a>
+                    </div>
+                  </div>
+                  
+                  <p class="u-text u-text-3 btn btn-secondary"><b>الخطة الدراسية</b></p>
+                  <div class="u-expanded-height-sm u-expanded-height-xs u-palette-4-base u-shape u-shape-rectangle u-shape-1"></div>
+                </div>
+              </div>
+
+
+              <div class="u-align-left u-container-style u-custom-item u-list-item u-repeater-item u-white u-list-item-1 ">
+                <div class="u-container-layout u-similar-container u-container-layout-1"><span class="u-align-left u-border-2 u-border-palette-2-base u-icon u-icon-circle u-spacing-24 u-text-custom-color-1 u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 24 24" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-29d3"></use></svg><svg class="u-svg-content" viewBox="0 0 24 24" id="svg-29d3"><path d="m12 6c-1.228 0-12-.084-12-3s10.772-3 12-3 12 .084 12 3-10.772 3-12 3zm-10.412-3c.732.568 4.245 1.5 10.412 1.5s9.68-.932 10.412-1.5c-.732-.568-4.245-1.5-10.412-1.5s-9.68.932-10.412 1.5zm20.939.116h.01z"></path><path d="m12 12c-1.228 0-12-.084-12-3 0-.414.336-.75.75-.75.385 0 .702.29.745.664.462.553 4.012 1.586 10.505 1.586s10.043-1.033 10.505-1.586c.043-.374.36-.664.745-.664.414 0 .75.336.75.75 0 2.916-10.772 3-12 3zm10.5-3.001c0 .001 0 .001 0 0zm-21 0c0 .001 0 .001 0 0z"></path><path d="m12 18c-1.228 0-12-.084-12-3 0-.414.336-.75.75-.75.385 0 .702.29.745.664.462.553 4.012 1.586 10.505 1.586s10.043-1.033 10.505-1.586c.043-.374.36-.664.745-.664.414 0 .75.336.75.75 0 2.916-10.772 3-12 3zm10.5-3.001c0 .001 0 .001 0 0zm-21 0c0 .001 0 .001 0 0z"></path><path d="m12 24c-1.228 0-12-.084-12-3v-18c0-.414.336-.75.75-.75s.75.336.75.75v17.919c.481.556 4.03 1.581 10.5 1.581s10.019-1.025 10.5-1.581v-17.919c0-.414.336-.75.75-.75s.75.336.75.75v18c0 2.916-10.772 3-12 3z"></path><circle cx="5" cy="14" r="1"></circle><circle cx="5" cy="8" r="1"></circle><circle cx="5" cy="20" r="1"></circle></svg></span>
+                  <h3 class="u-text u-text-default-lg u-text-default-md u-text-default-xl u-text-2">هندسة البرمجيات</h3>
+                  <p class="u-text u-text-3">
+                    تتمحور بشكل أساسي حول الصناعة البرمجية عالية الجودة بأسلوب منهجي ومنضبط وفعال . وبالتالي فإن هناك تأكيدة خاصة على التحليل والتقييم ، والمواصفات ، والتصميم ، والتنفيذ ، والصيانة وتطور البرمجية . وإلى جانب ذلك تلعب قضايا أخرى دورة حيوية في هندسة البرمجيات تتعلق بالإدارة والجودة ، الإبداع والابتكار ، المعايير المهارات الفردية ، العمل الجماعي والممارسات المهنية&nbsp;</p>
+                  <div class="dropdown u-text u-text-3">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <b>المجالات الوظيفية</b>
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a class="dropdown-item" href="#">مطور تطبيقات</a>
+                      <a class="dropdown-item" href="#">محلل بيانات</a>
+                      <a class="dropdown-item" href="#">مهندس شبكات</a>
+                      <a class="dropdown-item" href="#">مهندس برمجيات</a>
+                    </div>
+                  </div>
+                  
+                  <p class="u-text u-text-3 btn btn-secondary"><b>الخطة الدراسية</b></p>
+                  <div class="u-expanded-height-sm u-expanded-height-xs u-palette-4-base u-shape u-shape-rectangle u-shape-1"></div>
+                </div>
+              </div>
+
+
+
+            </div>
+          </div>
+        </div>
+      </section>
+</body>
+</html>
